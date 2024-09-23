db4f9213079c8d65ea71200843bc03a613014393
Miguel Gabriel <133173590+MiguelGabriel1613@users.noreply.github.com>
2024-09-23 08:34:28 -0300

    js var titulos / imc

--- pricipal.js
+++ pricipal.js
@@ -1,2 +1,8 @@
-var titulo = document.querySelector("h1");
+var titulo = document.querySelector('h1');
 titulo.textContent = "aparecida Nutricionista";
+
+var dadosPaulo = document.querySelector ('#linhaPaulo');
+var peso = dadosPaulo.querySelector('.info-peso').textContent;
+var altura = dadosPaulo.querySelector('.info-altura').textContent;
+var imc = peso / (altura * altura);
+dadosPaulo.querySelector('.info-imc').textContent;
