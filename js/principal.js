bfcad8d38006bfa2ad246b81a5eddfb4117eeda0
Miguel Gabriel <133173590+MiguelGabriel1613@users.noreply.github.com>
2024-09-25 08:19:27 -0300

    .

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,9 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = "aparecida Nutricionista";
 
-var pacientes = document.querySelectorAll('paciente');
+var pacientes = document.querySelectorAll('.paciente');
 for (let i = 0; i < pacientes.length; i++){
-    var peso = pacientes[i].querySelector('info-peso').textContent;
+    var peso = pacientes[i].querySelector('.info-peso').textContent;
     var altura = pacientes[i].querySelector ('.info-altura').textContent;
     var imc = peso / (altura*altura);
     pacientes[i].querySelector('.info-imc').textContent = imc;
