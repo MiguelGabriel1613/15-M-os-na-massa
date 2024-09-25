142de6bbf356cf7179affd327874d03d1c9a192b
Miguel Gabriel <133173590+MiguelGabriel1613@users.noreply.github.com>
2024-09-25 08:04:44 -0300

    for imc

--- pricipal.js
+++ pricipal.js
@@ -9,5 +9,9 @@ dadosPaulo.querySelector('.info-imc').textContent;
 
 var pacientes = document.querySelectorAll('paciente');
 for (let i = 0; i < pacientes.length; i++){
-    var peso = pacientes[i]
+    var peso = pacientes[i].querySelector('info-peso').textContent;
+    var altura = pacientes[i].querySelector ('.info-altura').textContent;
+    var imc = peso / (altura*altura);
+    pacientes[i].querySelector('.info-imc').textContent = imc;
+        
 }
